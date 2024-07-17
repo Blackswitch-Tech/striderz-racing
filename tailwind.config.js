160fcf8b61b9e06f835acbe303d6a8cd32bf8b9f
Ashish George <ashishgeorge2003@gmail.com>
2024-07-17 20:17:03 +0530

    feat: created teampage

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'exo-2': ['"Exo 2"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
