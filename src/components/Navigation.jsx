4270068f9d635fb80d7c978027486d846892064f
Jefrint <jefrintm@gmail.com>
2024-07-18 13:27:53 +0530

    moblie nav bar deisgn change

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -79,27 +79,38 @@ const Navigation = () => {
         </div>
       </nav>
 
-      <div className="fixed top-0 right-0 p-2 md:hidden z-50">
-        <button onClick={toggleOverlay} className="text-white text-3xl">
+      <div className="fixed top-0 left-0 md:hidden z-50 flex justify-between items-center w-full px-4 bg-black bg-opacity-20 backdrop-filter backdrop-blur-lg">
+        {!overlayVisible && (
+          <Link to="/" onClick={handleLogoClick}>
+            <motion.img
+              src={tsrLogo}
+              alt="TSR Logo"
+              className="h-16 w-auto cursor-pointer"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            />
+          </Link>
+        )}
+        <button onClick={toggleOverlay} className="text-white text-3xl opacity-80 hover:opacity-100 transition-opacity duration-300 ml-auto">
           {overlayVisible ? <FaTimes /> : <FaBars />}
         </button>
       </div>
 
       {overlayVisible && (
-        
         <div className="fixed inset-0 bg-black bg-opacity-95 z-40 flex flex-col items-center justify-center text-white">
           <motion.div className="flex items-center justify-center" style={{ height: '40px', overflow: 'visible' }}>
-                <Link to="/" onClick={toggleOverlay}>
-                  <motion.img
-                    src={tsrLogo}
-                    alt="TSR Logo"
-                    className="h-20 w-auto cursor-pointer mb-10"
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }}
-                  />
-                </Link>
-              </motion.div>
-
+            <Link to="/" onClick={toggleOverlay}>
+              <motion.img
+                src={tsrLogo}
+                alt="TSR Logo"
+                className="h-20 w-auto cursor-pointer mb-10"
+                initial={{ x: -100, y: -200 }}
+                animate={{ x: 0, y: 0 }}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+              />
+            </Link>
+          </motion.div>
           <NavLink to="/team" onClick={toggleOverlay}>Team</NavLink>
           <NavLink to="/journey" onClick={toggleOverlay}>Journey</NavLink>
           <NavLink to="/about" onClick={toggleOverlay}>About</NavLink>
