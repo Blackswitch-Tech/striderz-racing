79b52ad75a08488600886694a2d6353d7c94dc8d
Ashish George <ashishgeorge2003@gmail.com>
2024-07-18 15:07:25 +0530

    fix: journey, gallery loading

--- src/pages/JourneyPage.jsx
+++ src/pages/JourneyPage.jsx
@@ -1,162 +1,48 @@
-import React from 'react';
-import {
-  Timeline,
-  TimelineItem,
-  TimelineConnector,
-  TimelineHeader,
-  TimelineIcon,
-  TimelineBody,
-  Typography,
-} from "@material-tailwind/react";
-
-export default function DefaultTimeline() {
+import React from "react";
+import { motion } from "framer-motion";
+import Section from "../components/Section";
+import timelineData from "../components/journey.json";
+import { Typography } from "@material-tailwind/react";
+const JourneyPage = () => {
   return (
-    <div className="bg-[#0033CC] min-h-screen flex flex-col items-center justify-center pt-20 md:px-4">
-      <div className="w-full md:px-0">
-        <h1 className='flex items-center justify-center pb-6 text-white text-3xl font-bold'>Our Journey</h1>
-        <Typography className='md:px-64 lg:px-64 pb-8 text-center'>
-  TEAM STRIDERZ Racing began its journey at Saintgits College of Engineering, born from the Mechanical Engineering Department. Driven by a shared passion for racing and engineering, our team was formed to push the limits of innovation and performance.
-</Typography>
+    <div className="min-h-screen bg-[#0033CC] text-white ">
+      <Section>
+        <motion.h5
+          className="text-5xl font-bold text-center mb-8"
+          initial={{ opacity: 0, y: 0 }}
+          animate={{ opacity: 1, y: 1 }}
+          transition={{ duration: 1 }}
+        >
+          Our Journey
+        </motion.h5>
+        {/* Add team member cards or grid here */}
 
-        <Timeline className='md:px-64 lg:px-96   px-10 py-4'>
-        
-          <TimelineItem>
-         
-          <TimelineConnector className="bg-white !w-[3px]  !ml-1.5" />
-          <TimelineHeader className="flex items-center h-3">
-              <TimelineIcon className="bg-white text-white p-2 rounded-full"
-              
-               />
-              <Typography variant="h6" color="blue-gray" className="leading-none pl-4">
-                Inception
-              </Typography>
-            </TimelineHeader>
-            <TimelineBody className="pb-8">
-            <Typography variant="small" color="gray" className="font-thin white">
-            First team registered, cleared virtuals but did not participate in the event.
-              </Typography>
-            </TimelineBody>
-          </TimelineItem>
-          <TimelineItem>
-          <TimelineConnector className="bg-white !w-[3px] !ml-1.5" />
-          <TimelineHeader className="flex items-center h-3">
-              <TimelineIcon className=" text-white p-2 rounded-full bg-white" />
-              <Typography variant="h6" color="blue-gray" className="leading-none pl-4">
-                SAE Effi-cycle
-              </Typography>
-            </TimelineHeader>
-            <TimelineBody className="pb-8">
-            <Typography variant="small" color="gray" className="font-thin white ">
-            Successfully participated in the 2014 virtual round hosted at RIT Chennai on 28-29 June 2014.
-              </Typography>
-            </TimelineBody>
-          </TimelineItem>
-          <TimelineItem>
-          <TimelineConnector className="bg-white !w-[3px]  !ml-1.5" />
-          <TimelineHeader className="flex items-center h-3">
-          <TimelineIcon className=" text-white p-2 rounded-full bg-white" />
-          <Typography variant="h6" color="blue-gray" className="leading-none pl-4">
-                Car #113
-              </Typography>
-            </TimelineHeader>
-            <TimelineBody className="pb-8">
-            <Typography variant="small" color="gray" className="font-thin white">
-            -First team from Saintgits to clear external technical evaluation and qualify for the main event.
-            <Typography/>
-            <Typography variant="small" color="gray" className="font-thin white"></Typography>
-                -First team to clear Technical Inspection and virtuals to participate in the final race.
-              </Typography>
-            </TimelineBody>
-          </TimelineItem>
-          <TimelineItem>
-          <TimelineConnector className="bg-white !w-[3px]  !ml-1.5" />
-          <TimelineHeader className="flex items-center h-3">
-          <TimelineIcon className=" text-white p-2 rounded-full bg-white" />
-          <Typography variant="h6" color="blue-gray" className="leading-none pl-4">
-                Car #40
-              </Typography>
-            </TimelineHeader>
-            <TimelineBody className="pb-8">
-            <Typography variant="small" color="gray" className="font-normal white">
-            Achieved significant weight reduction from 325kg to 239.5kg.
-              </Typography>
-            </TimelineBody>
-          </TimelineItem>
-          <TimelineItem>
-          <TimelineConnector className="bg-white !w-[3px]  !ml-1.5" />
-          <TimelineHeader className="flex items-center h-3">
-          <TimelineIcon className=" text-white p-2 rounded-full bg-white" />
-          <Typography variant="h6" color="blue-gray" className="leading-none pl-4">
-                Car #89
-              </Typography>
-            </TimelineHeader>
-            <TimelineBody className="pb-8">
-              <Typography variant="small" color="gray" className="font-thin white">
-                -Completed 11 laps in the endurance race and recorded the fastest acceleration time (6.49 seconds) among Kerala teams.
-               
-              </Typography>
-              <Typography variant="small" color="gray" className="font-thin white">
-              -First time achieving a kerb weight below 200 kg.
-               
-              </Typography>
-              
-            </TimelineBody>
-          </TimelineItem>
-          <TimelineItem>
-          <TimelineConnector className="bg-white !w-[3px]  !ml-1.5" />
-            <TimelineHeader className="flex items-center h-3">
-            <TimelineIcon className=" text-white p-2 rounded-full bg-white" />
-            <Typography variant="h6" color="blue-gray" className="leading-none pl-4">
-                Car #83
-              </Typography>
-            </TimelineHeader>
-            <TimelineBody className="pb-8">
-            <Typography variant="small" color="gray" className="font-thin white">
-            -SAE BAJA: Secured first prize in the SLED PULL event, marking the first podium finish for a Kerala team and received a cash prize of Rs 60,000.
-               
-              </Typography>
-              <Typography variant="small" color="gray" className="font-thin white">
-              -Overall first position in ATV Competition held in IHRD Adoor, where all BAJA teams from Kerala competed.              </Typography>
-            </TimelineBody>
-          </TimelineItem>
-
-          <TimelineItem>
-          <TimelineConnector className="bg-white !w-[3px]  !ml-1.5" />
-          <TimelineHeader className="flex items-center h-3">
-          <TimelineIcon className=" text-white p-2 rounded-full bg-white" />
-          <Typography variant="h6" color="blue-gray" className="leading-none pl-4">
-                Car #113
-              </Typography>
-            </TimelineHeader>
-            <TimelineBody className="pb-8">
-            <Typography variant="small" color="gray" className="font-thin white">
-            -First team from Saintgits to clear external technical evaluation and qualify for the main event.
-            <Typography/>
-            <Typography variant="small" color="gray" className="font-normal white"></Typography>
-                -First team to clear Technical Inspection and virtuals to participate in the final race.
-              </Typography>
-            </TimelineBody>
-          </TimelineItem>
-          <TimelineItem>
-          <TimelineConnector className="bg-white !w-[3px]  !ml-1.5" />
-          <TimelineHeader className="flex items-center h-3">
-          <TimelineIcon className=" text-white p-2 rounded-full bg-white" />
-          <Typography variant="h6" color="blue-gray" className="leading-none pl-4">
-                Car #113
-              </Typography>
-            </TimelineHeader>
-            <TimelineBody className="pb-8">
-            <Typography variant="small" color="gray" className="font-thin white">
-            -First team from Saintgits to clear external technical evaluation and qualify for the main event.
-            <Typography/>
-            <Typography variant="small" color="gray" className="font-normal white"></Typography>
-                -First team to clear Technical Inspection and virtuals to participate in the final race.
-              </Typography>
-            </TimelineBody>
-          </TimelineItem>
-          
-        </Timeline>
-      </div>
+        <Typography className="md:px-64 lg:px-64 pb-8 text-center m-5 ">
+          TEAM STRIDERZ Racing began its journey at Saintgits College of
+          Engineering, born from the Mechanical Engineering Department. Driven
+          by a shared passion for racing and engineering, our team was formed to
+          push the limits of innovation and performance.
+        </Typography>
+        <ol className="relative border-l border-gray-200 dark:border-gray-700 m-5  md:w-6/12 md:mx-auto">
+          {timelineData.map((item, index) => (
+            <li className="mb-10 ml-4" key={index}>
+              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+              <time className="mb-1 text-sm font-normal leading-none text-white ">
+                {item.time}
+              </time>
+              <h3 className="text-lg font-semibold text-white">{item.title}</h3>
+              {item.image && (
+                <img src={item.image} alt={item.title} className="mb-4" />
+              )}
+              <p className="text-base font-normal text-white whitespace-pre-line">
+                {item.description}
+              </p>
+            </li>
+          ))}
+        </ol>
+      </Section>
     </div>
   );
-}
+};
+
+export default JourneyPage;
