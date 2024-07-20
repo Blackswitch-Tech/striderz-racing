fc26a617df5755140b97a00a456d7e95bb2d7d09
fredysomy <fredysomy@gmail.com>
2024-07-20 22:58:13 +0530

    mod: some new changes

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/team" element={<TeamPage />} />
           <Route path="/journey" element={<JourneyPage />} />
-          <Route path="/about" element={<AboutPage />} />
 
           <Route path="/contact-us" element={<ContactPage />} />
           <Route path="/gallery" element={<Gallery />} />
